315ee2f46c4608f4a7b2c78b328d889fecc19863
damufe <damufe92@gmail.com>
2026-05-03 19:05:13 +0200

    Configuración inicial de Terraform con validación CI

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = ">= 1.0"   # Control de versiones de Terraform
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"        # Version AWS
+    }
+  }
+}
+
+# Configuración del proveedor AWS apuntando a LocalStack
+provider "aws" {
+  region                      = "eu-west-1"
+  access_key                  = "test"                # credenciales
+  secret_key                  = "test"
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  skip_metadata_api_check     = true
+  s3_use_path_style           = true                 # necesario para S3 en LocalStack
+
+  endpoints {
+    s3 = "http://localhost:4566"   # endpoint de LocalStack
+  }
+}
